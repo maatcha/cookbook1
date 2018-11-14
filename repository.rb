c447c423abe202176731a5fa4691368f85c16fd9
maatcha <mattcha54@gmail.com>
2018-11-14 12:12:53 +0100

    base without csv

--- repository.rb
+++ repository.rb
@@ -1,12 +1,7 @@
 class Cookbook
 	
 	def initialize(data)
-		load_csv
 		@recipes = []
-	end
-
-	def load_csv
-		@next_id = @recipes.empty? ? 1 : @recipes.last.id + 1
 	end
 
 	def all
@@ -14,10 +9,7 @@ class Cookbook
 	end
 
 	def add_recipe(recipe)
-		recipe.id = @next_id
-		@next_id += 1
 		@recipes << recipe
-		save_csv
 	end
 
 	def remove_recipe(index)
